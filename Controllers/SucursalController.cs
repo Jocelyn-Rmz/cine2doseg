ea3105f8aa3c4eb70f2b0df5fe469a4b79e39e27
JoseBDD05 <l21200648@pachuca.tecnm.mx>
2025-03-24 22:36:51 -0600

    Version Inicial

---
+++ Controllers/SucursalController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using cine2doseg.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+//------------------------
+
+//LUZ MARIA CRUZ GARCIA
+//JOCELYN RAMIREZ PEREZ
+//JOSE ANTONIO FUENTES RAMIREZ
+namespace cine2doseg.Controllers
+{
+    public class SucursalController : ApiController
+    {
+        [HttpGet]
+        [Route("cine/sucursal/vwrptsucursales")]
+        public clsApiStatus vwRptSucursales()
+        {
+            clsApiStatus respuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                clsSucursal sucursal = new clsSucursal();
+                ds = sucursal.vwRptSucursales();
+                respuesta.statusExec = true;
+                respuesta.ban = ds.Tables[0].Rows.Count;
+                respuesta.msg = "Sucursales consultadas correctamente";
+                jsonResp["sucursales"] = JToken.FromObject(ds.Tables[0]);
+                respuesta.datos = jsonResp;
+            }
+            catch (Exception ex)
+            {
+                respuesta.statusExec = false;
+                respuesta.ban = -1;
+                respuesta.msg = "Error al consultar sucursales";
+                jsonResp["msgData"] = ex.Message;
+                respuesta.datos = jsonResp;
+            }
+
+            return respuesta;
+        }
+
+        [HttpPost]
+        [Route("cine/sucursal/spinssucursales")]
+        public clsApiStatus spInsSucursales([FromBody] clsSucursal modelo)
+        {
+            clsApiStatus respuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                clsSucursal sucursal = new clsSucursal(modelo.Nombre, modelo.Direccion, modelo.Url, modelo.Logo);
+                ds = sucursal.spInsSucursales();
+                respuesta.statusExec = true;
+                respuesta.ban = int.Parse(ds.Tables[0].Rows[0][0].ToString());
+                respuesta.msg = respuesta.ban == 0 ? "Sucursal registrada exitosamente" : "Error en la inserción";
+                jsonResp["msgData"] = respuesta.msg;
+                respuesta.datos = jsonResp;
+            }
+            catch (Exception ex)
+            {
+                respuesta.statusExec = false;
+                respuesta.ban = -1;
+                respuesta.msg = "Error al insertar sucursal";
+                jsonResp["msgData"] = ex.Message;
+                respuesta.datos = jsonResp;
+            }
+
+            return respuesta;
+        }
+    }
+}
---
+++ Models/clsSucursal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// -----------------------------
+using System.Configuration;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+
+//LUZ MARIA CRUZ GARCIA
+//JOCELYN RAMIREZ PEREZ
+//JOSE ANTONIO FUENTES RAMIREZ
+// Modelo: clsSucursal.cs
+namespace cine2doseg.Models
+{
+    public class clsSucursal
+    {
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Url { get; set; }
+        public string Logo { get; set; }
+
+        string cadConn = ConfigurationManager.ConnectionStrings["bdCine"].ConnectionString;
+
+        public clsSucursal() { }
+
+        public clsSucursal(string nombre, string direccion, string url, string logo)
+        {
+            this.Nombre = nombre;
+            this.Direccion = direccion;
+            this.Url = url;
+            this.Logo = logo;
+        }
+
+        public DataSet vwRptSucursales()
+        {
+            string query = "SELECT * FROM vwRptSucursales";
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(query, cnn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "vwRptSucursales");
+            return ds;
+        }
+
+        public DataSet spInsSucursales()
+        {
+            string cadSql = "CALL spInsSucursales('" + this.Nombre +
+                                    "', '" + this.Direccion +
+                                    "', '" + this.Url +
+                                    "', '" + this.Logo + "')";
+
+            MySqlConnection cnn = new MySqlConnection(cadConn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cadSql, cnn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "spInsSucursales");
+            return ds;
+        }
+    }
+}
+
